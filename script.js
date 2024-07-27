53c2fa9f4f20c6725d7fd39bd5a47c5e99e2f04e
Wisnu K. Ardianto <setrasariputra@gmail.com>
2024-07-27 17:05:15 +0700

    Sample Conditinonal JS

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-alert('tes js from file')
+var numberOne = 10; // integer
+var numberTwo = 6; // integer
+var myName = "Wisnu Kuncoro"; // string
+var score = 5;
+
+// conditional if else
+// if(score > 7) {
+//     alert('Lulus')
+// }else{
+//     alert('Tidak lulus')
+// }
+
+
+document.querySelector('.mobile-menu-trigger').addEventListener('click', function() {
+    // show responsive menu
+    var menu = document.querySelector('.topmenu ul');
+    if(menu.style.display === 'block') {
+        menu.style.display = 'none';
+    }else{
+        menu.style.display = 'block';
+    }
+});
